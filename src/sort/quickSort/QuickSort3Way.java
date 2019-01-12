a544c5aef9e2c7bdd4508f6e8fc4ea97eefbcff1
IvanLu1024 <501275190@qq.com>
2019-01-12 20:29:38 +0800

    三路快排

--- src/sort/quickSort/QuickSort3Way.java
+++ src/sort/quickSort/QuickSort3Way.java
@@ -1,6 +1,8 @@
 package sort.quickSort;
 
 import org.junit.Test;
+import sort.Sort;
+import utils.SortTestHelper;
 
 import java.util.Collections;
 
@@ -10,18 +12,20 @@ import java.util.Collections;
  * 在有大量重复元素的排序过程中效果很好
  *
  */
-public class QuickSort3Way {
+public class QuickSort3Way<T extends Comparable<T>> extends Sort<T> {
 
-    public void sort(Comparable[] a,int l,int h){
+    public void sort(T[] a,int l,int h){
         if (h<=l)
             return;
         int lt=l,i=l+1,gt=h;//分别标注小于中位数的，等于中位数的，大于中位数的下标
-        Comparable v = a[l];
+        //partition
+        T v = a[l];
         while (i<=gt){
             int cmp = a[i].compareTo(v);
             if (cmp<0){
                swap(a,lt++,i++);
             }else if (cmp>0){
+                //需要注意这里的i是未遍历的元素，所以不需要加1操作
                 swap(a,i,gt--);
             }else i++;
         }
@@ -30,19 +34,19 @@ public class QuickSort3Way {
         sort(a,gt+1,h);
 
     }
-
-    private void swap(Comparable[] a,int i,int j){
-        Comparable t = a[i];
-        a[i]=a[j];
-        a[j]=t;
+    @Override
+    public void sort(T[] nums) {
+        sort(nums,0,nums.length-1);
     }
+
+
     @Test
     public void test(){
-        Comparable[] ints ={2,1,1,1,5,7,4,3};
-        sort(ints,0,ints.length-1);
-        for (int i=0;i<ints.length;i++){
-            System.out.print(ints[i]+" ");
-        }
-
+        Integer[] nums = SortTestHelper.generateRandomArray(10, 0, 10);
+        QuickSort3Way<Integer> sort = new QuickSort3Way<>();
+        sort.sort(nums);
+        SortTestHelper.printArray(nums);
     }
+
+
 }
